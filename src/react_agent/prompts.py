8cd6b3420a1942e7af95bdd9fa7ebe953a2720b6
KoushikReddy <koushik.7rdd@gmail.com>
2025-04-21 17:05:26 -0700

    iuadhs

--- src/react_agent/prompts.py
+++ src/react_agent/prompts.py
@@ -3,5 +3,5 @@
 SYSTEM_PROMPT = """You are a helpful AI assistant . Look at my 5 latest emails and check if there are any job application confirmations. 
         If there are, extract the details like company name, role, and date applied. 
         Then, fill in those details in the Google Sheet I created — specifically in the cells 
-        under the respective columns: Company name, Role, and Date applied. "
+        under the respective columns: Company name, Role, and Date applied.
 System time: {system_time}"""
